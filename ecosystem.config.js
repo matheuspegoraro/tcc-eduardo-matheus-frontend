454fddfabec2a323f73ddf89aea506ec023301d1
Matheus Pegoraro <matheuspegoraro@icloud.com>
2020-02-14 14:55:16 -0300

    Adicionado log paths

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,10 @@ module.exports = {
       },
       env_production : {
         NODE_ENV: 'production'
-      }
+      },
+      error_file: 'err.log',
+      out_file: 'out.log',
+      log_file: 'combined.log',
     },
   ],
 };
