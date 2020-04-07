93a7d8e687cbf265952375142191237e5f452086
Matheus Pegoraro <matheuspegoraro@icloud.com>
2020-04-06 21:14:48 -0300

    Adicionando nome vindo do token no sidebar

--- src/components/Navbars/DashboardNavbar.jsx
+++ src/components/Navbars/DashboardNavbar.jsx
@@ -18,68 +18,68 @@ import {
   Media
 } from "reactstrap";
 
-import { removeToken } from "../../auth";
+import { removeToken, getTokenDecoded } from "../../auth";
 
-class DashboardNavbar extends React.Component {
+function DashboardNavbar() {
 
-  logout = async (e) => {
+  const userName = getTokenDecoded().name;
+
+  const logout = async (e) => {
     e.preventDefault();
     removeToken();
   }
 
-  render() {
-    return (
-      <>
-        <Navbar className="navbar-top navbar-dark" expand="md" id="navbar-main">
-          <Container fluid>
-            <Form className="navbar-search navbar-search-dark form-inline mr-3 d-none d-md-flex ml-lg-auto">
-            </Form>
-            <Nav className="align-items-center d-none d-md-flex" navbar>
-              <UncontrolledDropdown nav>
-                <DropdownToggle className="pr-0" nav>
-                  <Media className="align-items-center">
-                    <span className="avatar avatar-sm rounded-circle">
-                      <img
-                        alt="..."
-                        src={require("assets/img/theme/team-1-800x800.jpg")}
-                      />
+  return (
+    <>
+      <Navbar className="navbar-top navbar-dark" expand="md" id="navbar-main">
+        <Container fluid>
+          <Form className="navbar-search navbar-search-dark form-inline mr-3 d-none d-md-flex ml-lg-auto">
+          </Form>
+          <Nav className="align-items-center d-none d-md-flex" navbar>
+            <UncontrolledDropdown nav>
+              <DropdownToggle className="pr-0" nav>
+                <Media className="align-items-center">
+                  <span className="avatar avatar-sm rounded-circle">
+                    <img
+                      alt="..."
+                      src={require("assets/img/theme/team-1-800x800.jpg")}
+                    />
+                  </span>
+                  <Media className="ml-2 d-none d-lg-block">
+                    <span className="mb-0 text-sm font-weight-bold">
+                      {userName}
                     </span>
-                    <Media className="ml-2 d-none d-lg-block">
-                      <span className="mb-0 text-sm font-weight-bold">
-                        Matheus Pegoraro
-                      </span>
-                    </Media>
                   </Media>
-                </DropdownToggle>
-                <DropdownMenu className="dropdown-menu-arrow" right>
-                  <DropdownItem className="noti-title" header tag="div">
-                    <h6 className="text-overflow m-0">Bem vindo!</h6>
-                  </DropdownItem>
-                  <DropdownItem to="/app/user-profile" tag={Link}>
-                    <i className="ni ni-single-02" />
-                    <span>Perfil</span>
-                  </DropdownItem>
-                  <DropdownItem to="/app/user-profile" tag={Link}>
-                    <i className="ni ni-settings-gear-65" />
-                    <span>Configurações</span>
-                  </DropdownItem>
-                  <DropdownItem to="/app/user-profile" tag={Link}>
-                    <i className="ni ni-calendar-grid-58" />
-                    <span>Atividades</span>
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem href="#" onClick={this.logout}>
-                    <i className="ni ni-user-run" />
-                    <span>Sair</span>
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-          </Container>
-        </Navbar>
-      </>
-    );
-  }
+                </Media>
+              </DropdownToggle>
+              <DropdownMenu className="dropdown-menu-arrow" right>
+                <DropdownItem className="noti-title" header tag="div">
+                  <h6 className="text-overflow m-0">Bem vindo!</h6>
+                </DropdownItem>
+                <DropdownItem to="/app/user-profile" tag={Link}>
+                  <i className="ni ni-single-02" />
+                  <span>Perfil</span>
+                </DropdownItem>
+                <DropdownItem to="/app/user-profile" tag={Link}>
+                  <i className="ni ni-settings-gear-65" />
+                  <span>Configurações</span>
+                </DropdownItem>
+                <DropdownItem to="/app/user-profile" tag={Link}>
+                  <i className="ni ni-calendar-grid-58" />
+                  <span>Atividades</span>
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem href="#" onClick={logout}>
+                  <i className="ni ni-user-run" />
+                  <span>Sair</span>
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
 export default DashboardNavbar;
